4b6b80b3c64d549678e951d625800e2ad577a05c
demirbilek95 <dogancandemirbilek@gmail.com>
2020-12-30 13:47:15 +0100

    redundant System.out.println() is deleted in constructor

--- src/main/java/sdmExam/Board.java
+++ src/main/java/sdmExam/Board.java
@@ -7,12 +7,11 @@ public class Board {
     private List<Cell> _plays = new ArrayList<>();
 
     public Board(){
-        // Board is 13x13, but we can use edges and corners mark so it is 14x14
+        // Board is 13x13 squares, but we can use edges and corners to mark so it is 14x14 cells
         for (int i = 0; i < 14; i++){
             for (int j = 0; j < 14; j++){
                 Mark mark = Mark.NONE;
                 Position position = new Position(i,j);
-                System.out.println(position);
                 Cell cell = new Cell(position,mark);
                 _plays.add(cell);
             }
