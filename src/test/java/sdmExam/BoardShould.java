d6c43f30ad72f2deee1d01717464a13c14fb92f1
Eros Fabrici <eros.fabrici@gmail.com>
2021-01-02 16:36:00 +0100

    renamed test methods

--- src/test/java/sdmExam/BoardShould.java
+++ src/test/java/sdmExam/BoardShould.java
@@ -10,18 +10,18 @@ public class BoardShould {
     private final Board board = new Board();
 
     @Test
-    public void getCorrectCellGivenAPosition() {
+    public void getCorrectIntersectionGivenAPosition() {
         Position position = Position.in(5, 7);
         assertTrue(board.cellAt(position).isPresent());
     }
 
     @Test
-    public void cellOutsideBoard() {
+    public void intersectionOutsideBoard() {
         assertTrue(board.cellAt(Position.in(14, 14)).isEmpty());
     }
 
     @Test
-    public void markCorrectlyACell() {
+    public void markCorrectlyAnIntersection() {
         Position position = new Position(5, 7);
         Intersection intersection = board.cellAt(position).get();
         board.addMarkAt(Mark.BLACK, position);
--- src/test/java/sdmExam/IntersectionShould.java
+++ src/test/java/sdmExam/IntersectionShould.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntersectionShould {
     @Test
-    public void beEqualToAnotherCellWithEqualFields() {
+    public void beEqualToAnotherIntersectionWithEqualFields() {
         Intersection firstIntersection = new Intersection(Position.in(3, 3), Mark.WHITE);
         Intersection secondIntersection = new Intersection(Position.in(3, 3), Mark.WHITE);
         assertEquals(firstIntersection, secondIntersection);
