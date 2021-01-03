1f02f8db6134ebd61f04399eda26963d0ed80333
wilsongefferson <pierpa.m.96live.it>
2021-01-03 17:26:23 +0100

    Game.play refactor.

--- src/main/java/sdmExam/Game.java
+++ src/main/java/sdmExam/Game.java
@@ -4,15 +4,11 @@ public class Game {
     private final Board board;
     private Mark player1 = Mark.BLACK;
     private Mark player2 = Mark.WHITE;
-    private boolean firstPlay = true;
-    private Mark lastPlayer;
+    private Mark lastPlayer = Mark.WHITE;
 
     public Game(){this.board = new Board();}
 
     public void play(Mark player, Position position) throws Exception{
-        if(firstPlay && player == this.player2){
-            throw new Exception();
-        }
 
         if(player == lastPlayer){
             throw new Exception();
