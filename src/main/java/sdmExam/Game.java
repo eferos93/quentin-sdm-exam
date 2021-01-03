b6ad14326e69f40f2b8c9ef078b7b0fd4a8eaaa6
wilsongefferson <pierpa.m.96live.it>
2021-01-03 19:15:31 +0100

    removed dead fields and removed constructor in Game class.

--- src/main/java/sdmExam/Game.java
+++ src/main/java/sdmExam/Game.java
@@ -1,14 +1,8 @@
 package sdmExam;
 
-import java.security.spec.ECField;
-
 public class Game {
-    private final Board board;
-    private Mark player1 = Mark.BLACK;
-    private Mark player2 = Mark.WHITE;
+    private final Board board = new Board();
     private Mark lastPlayer = Mark.WHITE;
-
-    public Game(){this.board = new Board();}
 
     public void play(Mark player, Position position) throws Exception{
 
