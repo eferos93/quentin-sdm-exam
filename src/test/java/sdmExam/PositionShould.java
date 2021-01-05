0c34374c1e6d4f2c4102fb63d1fa02c41ef01f50
pasc <apascariu32@gmail.com>
2021-01-05 22:25:38 +0100

    Write a test for Position.isAboveWithRespectTo and changed again those 2 previous methods

--- src/test/java/sdmExam/PositionShould.java
+++ src/test/java/sdmExam/PositionShould.java
@@ -3,6 +3,7 @@ package sdmExam;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PositionShould {
     @Test
@@ -11,6 +12,11 @@ public class PositionShould {
         Position secondPosition = new Position(3, 3);
         assertEquals(firstPosition, secondPosition);
     }
-
+    @Test
+    public void stoneAbove(){
+        Position firstPosition = new Position(1, 2);
+        Position secondPosition = new Position (2,2);
+        assertTrue(firstPosition.isAboveWithRespectTo(secondPosition));
+    }
 
 }
