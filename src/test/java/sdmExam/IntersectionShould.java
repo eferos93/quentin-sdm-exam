228ffca2a8ac0e7917d14964b269d17b1d15dacf
demirbilek95 <dogancandemirbilek@gmail.com>
2021-01-09 13:25:44 +0100

    unused imports are removed

--- src/test/java/sdmExam/IntersectionShould.java
+++ src/test/java/sdmExam/IntersectionShould.java
@@ -1,9 +1,6 @@
 package sdmExam;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
