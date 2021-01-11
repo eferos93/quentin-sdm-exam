c8ee49e843443a8385227f49ff26f1ba1e8da58a
Eros Fabrici <eros.fabrici@gmail.com>
2021-01-11 16:35:32 +0100

    Update IllegalMoveException.java
    made toString implicit

--- src/main/java/sdmExam/exceptions/IllegalMoveException.java
+++ src/main/java/sdmExam/exceptions/IllegalMoveException.java
@@ -4,7 +4,7 @@ import sdmExam.Position;
 
 public class IllegalMoveException extends Exception {
     public IllegalMoveException(Position position){
-        super("Player cannot put a stone diagonally adjacent to another stone of the same colour this" + position.toString() +
+        super("Player cannot put a stone diagonally adjacent to another stone of the same colour this" + position +
                 " without a colour alike orthogonally adjacent.");
     }
 }
