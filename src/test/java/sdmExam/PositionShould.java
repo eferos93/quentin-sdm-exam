3ee8fac2c997eda78b6ca707b1b3755ca404159b
demirbilek95 <dogancandemirbilek@gmail.com>
2021-01-12 17:10:23 +0100

    redundant tests are deleted from PositionShould

--- src/test/java/sdmExam/PositionShould.java
+++ src/test/java/sdmExam/PositionShould.java
@@ -17,41 +17,6 @@ public class PositionShould {
         assertEquals(firstPosition, secondPosition);
     }
 
-    @Test
-    public void stoneAbove() {
-        Position firstPosition = new Position(1, 2);
-        Position secondPosition = new Position(2, 2);
-        assertTrue(firstPosition.isAboveWithRespectTo(secondPosition));
-    }
-
-    @Test
-    public void stoneBelow() {
-        Position firstPosition = new Position(3, 4);
-        Position secondPosition = new Position(2, 2);
-        assertFalse(firstPosition.isBelowWithRespectTo(secondPosition));
-    }
-
-    @Test
-    public void stoneLeft() {
-        Position firstPosition = new Position(3, 4);
-        Position secondPosition = new Position(3, 5);
-        assertTrue(firstPosition.isOnTheLeftWithRespectTo(secondPosition));
-    }
-
-    @Test
-    public void stoneUpRight() {
-        Position firstPosition = new Position(5, 6);
-        Position secondPosition = new Position(6, 5);
-        assertTrue(firstPosition.isUpRightRespectTo(secondPosition));
-    }
-
-    @Test
-    public void stoneDownLeft() {
-        Position firstPosition = new Position(7, 12);
-        Position secondPosition = new Position(6, 13);
-        assertTrue(firstPosition.isDownLeftRespectTo(secondPosition));
-    }
-
     @TestFactory
     Collection<DynamicTest> checkPositions() {
         Position firstPosition = new Position(1, 2);
