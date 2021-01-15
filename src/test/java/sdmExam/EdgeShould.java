c54760421d709a7720d390b8169de84630fb9478
demirbilek95 <dogancandemirbilek@gmail.com>
2021-01-15 12:01:39 +0100

    removed redundant tests from EdgeShould

--- src/test/java/sdmExam/EdgeShould.java
+++ src/test/java/sdmExam/EdgeShould.java
@@ -11,29 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EdgeShould {
     private Board board = new Board();
-    @Test
-    public void returnTrueIfAboveToaGivenPosition() {
-        Edge edge = Edge.TOP;
-        assertTrue(edge.isAdjacentTo(Position.in(1, 4)));
-    }
-
-    @Test
-    public void returnTrueIfBelowToAGivenPosition() {
-        Edge edge = Edge.BOTTOM;
-        assertTrue(edge.isAdjacentTo(Position.in(13, 4)));
-    }
-
-    @Test
-    public void returnTrueIfOnTheLeftToAGivenPosition() {
-        Edge edge = Edge.LEFT;
-        assertTrue(edge.isAdjacentTo(Position.in(13, 1)));
-    }
-
-    @Test
-    public void returnTrueIfOnTheRightToAGivenPosition() {
-        Edge edge = Edge.RIGHT;
-        assertTrue(edge.isAdjacentTo(Position.in(1, 13)));
-    }
 
     @TestFactory
     Collection<DynamicTest> checkEdges() {
