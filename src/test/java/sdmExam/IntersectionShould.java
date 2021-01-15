014d568574d31f1bf9a09c1223522435b98941ce
demirbilek95 <dogancandemirbilek@gmail.com>
2021-01-15 15:01:24 +0100

    IntersectionShould.checkOrthogonalAdjacency and IntersectionShould.checkDiagonalAdjacency are written

--- src/test/java/sdmExam/IntersectionShould.java
+++ src/test/java/sdmExam/IntersectionShould.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +30,50 @@ public class IntersectionShould {
                 Arguments.of(new Intersection(Position.in(5, 12), Stone.WHITE)
                         ,new Intersection(Position.in(5, 12), Stone.WHITE))
         );
+    }
+
+    @TestFactory
+    Stream<DynamicTest> checkOrthogonalAdjacency(){
+        Intersection firstIntersection = new Intersection(Position.in(8,5), Stone.BLACK);
+        Intersection secondIntersection = new Intersection(Position.in(7,9), Stone.BLACK);
+        Intersection thirdIntersection = new Intersection(Position.in(3,12), Stone.BLACK);
+
+        Intersection firstParameter = new Intersection(Position.in(9,5), Stone.BLACK);
+        Intersection secondParameter = new Intersection(Position.in(7,3), Stone.BLACK);
+        Intersection thirdParameter = new Intersection(Position.in(3,12), Stone.BLACK);
+
+        List<Intersection> inputList = Arrays.asList(firstIntersection, secondIntersection,thirdIntersection);
+        List<Boolean> outputList = Arrays.asList(true, false, false);
+        List<Intersection> parameterList = Arrays.asList(firstParameter, secondParameter, thirdParameter);
+
+        return inputList.stream()
+                .map(intersection -> DynamicTest.dynamicTest("Checking Orthogonal Adjacent of "+ intersection,
+                        () -> {
+                            int id = inputList.indexOf(intersection);
+                            assertEquals(outputList.get(id),intersection.isOrthogonalTo(parameterList.get(id)));
+                        }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> checkDiagonalAdjacency(){
+        Intersection firstIntersection = new Intersection(Position.in(7,9), Stone.WHITE);
+        Intersection secondIntersection = new Intersection(Position.in(3,3), Stone.WHITE);
+        Intersection thirdIntersection = new Intersection(Position.in(3,12), Stone.WHITE);
+
+        Intersection firstParameter = new Intersection(Position.in(6,10), Stone.WHITE);
+        Intersection secondParameter = new Intersection(Position.in(4,4), Stone.WHITE);
+        Intersection thirdParameter = new Intersection(Position.in(5,1), Stone.WHITE);
+
+        List<Intersection> inputList = Arrays.asList(firstIntersection, secondIntersection,thirdIntersection);
+        List<Boolean> outputList = Arrays.asList(true, true, false);
+        List<Intersection> parameterList = Arrays.asList(firstParameter, secondParameter, thirdParameter);
+
+        return inputList.stream()
+                .map(intersection -> DynamicTest.dynamicTest("Checking Diagonal Adjacent of "+ intersection,
+                        () -> {
+                            int id = inputList.indexOf(intersection);
+                            assertEquals(outputList.get(id),intersection.isDiagonalTo(parameterList.get(id)));
+                        }));
     }
 
     //TODO Below test can be written with streams (by using object function tuples)
