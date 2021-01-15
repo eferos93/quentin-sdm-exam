cc6c5874073cfad20dbaef0a70b1c133757df7cd
Stefano <stefanosimonetto98@gmail.com>
2021-01-15 15:17:23 +0100

    Refactored main

--- src/main/java/sdmExam/Main.java
+++ src/main/java/sdmExam/Main.java
@@ -9,28 +9,17 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int coordinateX;
-
-        Game game = new Game();
         Board board = new Board();
 
         Graphics.printTitle();
         Graphics.Instructions();
         Graphics.BlackPlayerPlayFirst();
         PrintBoard.Print(board);
-        Graphics.ChooseCoordinateX();
-        coordinateX=scanner.nextInt();
-        Graphics.ChooseCoordinateY();
-        Graphics.SetCoordinates(board,coordinateX,scanner.nextInt());
+        Graphics.Set(Stone.BLACK);
         Graphics.Pie();
         if(scanner.nextInt()==1)
             Graphics.ApplyPie();
-        else{
-            Graphics.ChooseCoordinateX();
-            coordinateX=scanner.nextInt();
-            Graphics.ChooseCoordinateY();
-            Graphics.SetCoordinates(board,coordinateX,scanner.nextInt());
-        }
+        else{Graphics.Set(Stone.WHITE);}
     }
 
 }
--- src/main/java/sdmExam/UI/Graphics.java
+++ src/main/java/sdmExam/UI/Graphics.java
@@ -5,9 +5,13 @@ import sdmExam.Game;
 import sdmExam.Position;
 import sdmExam.Stone;
 
+import java.util.Scanner;
+
 public class Graphics {
+    private static int coordinateX;
+    private static Board board=new Board();
 
-
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void printTitle() {System.out.println(Message.TITLE);}
 
@@ -15,13 +19,18 @@ public class Graphics {
 
     public static void BlackPlayerPlayFirst() {System.out.println(Message.BLACKPLAYFIRST);}
 
-    public static void ChooseCoordinateX() {System.out.println(Message.CHOOSE_X);}
 
-    public static void ChooseCoordinateY() {System.out.println(Message.CHOOSE_Y);}
+    public static void Set(Stone stone) {
+        System.out.println(Message.CHOOSE_X);
+        coordinateX=scanner.nextInt();
+        System.out.println(Message.CHOOSE_Y);
+        SetCoordinates(board,stone,coordinateX,scanner.nextInt());
+    }
 
-    public static void SetCoordinates(Board board,int x, int y) {
+    public static void SetCoordinates(Board board,Stone stone,int x, int y) {
         Position position = new Position(x, y);
-        board.addStoneAt(Stone.BLACK, position);
+        board.addStoneAt(stone, position);
+        PrintBoard.Print(board);
     }
 
     public static void Pie() {System.out.println(Message.PIE);}
