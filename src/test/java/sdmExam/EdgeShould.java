ea8c1cb6bde253344827edc646deeb5c551d7ebc
demirbilek95 <dogancandemirbilek@gmail.com>
2021-01-15 12:01:16 +0100

    EdgeShould.checkEdges is written with Dynamic Testing

--- src/test/java/sdmExam/EdgeShould.java
+++ src/test/java/sdmExam/EdgeShould.java
@@ -1,6 +1,12 @@
 package sdmExam;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EdgeShould {
@@ -27,5 +33,25 @@ public class EdgeShould {
     public void returnTrueIfOnTheRightToAGivenPosition() {
         Edge edge = Edge.RIGHT;
         assertTrue(edge.isAdjacentTo(Position.in(1, 13)));
+    }
+
+    @TestFactory
+    Collection<DynamicTest> checkEdges() {
+
+        Edge edgeTop = Edge.TOP;
+        Edge edgeBottom = Edge.BOTTOM;
+        Edge edgeLeft = Edge.LEFT;
+        Edge edgeRight = Edge.RIGHT;
+
+        return Arrays.asList(
+                DynamicTest.dynamicTest("Return True If Above the Given Position",
+                        () -> assertTrue(edgeTop.isAdjacentTo(Position.in(1, 4)))),
+                DynamicTest.dynamicTest("Return True If Below the Given Position",
+                        () -> assertTrue(edgeBottom.isAdjacentTo(Position.in(13, 4)))),
+                DynamicTest.dynamicTest("Return True If Left the Given Position",
+                        () -> assertTrue(edgeLeft.isAdjacentTo(Position.in(13, 1)))),
+                DynamicTest.dynamicTest("Return True If Right the Given Position",
+                        () -> assertTrue(edgeRight.isAdjacentTo(Position.in(1, 13))))
+        );
     }
 }
