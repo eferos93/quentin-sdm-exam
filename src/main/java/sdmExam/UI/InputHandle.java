b661b479b8eba95f6bfbe660db1dc567a31110e5
Stefano <stefanosimonetto98@gmail.com>
2021-01-20 16:22:30 +0100

    Created new method in with a try-catch in order to avoid wrong input

--- src/main/java/sdmExam/UI/InputHandle.java
+++ src/main/java/sdmExam/UI/InputHandle.java
@@ -2,6 +2,7 @@ package sdmExam.UI;
 
 import sdmExam.UI.Exception.OutOfBoardException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputHandle   {
@@ -13,16 +14,32 @@ public class InputHandle   {
         System.out.println(Message.ASK_SIZE);
         int size;
         try {
-            size = scanner.nextInt();
+            size = getInteger();
             if (size < 3 || size > 11)
                 throw new OutOfBoardException(Message.INVALID_INPUT_SIZE);
 
-        } catch(OutOfBoardException error) {
+        } catch (OutOfBoardException error) {
             System.out.println(error.getMessage());
             return askSize();
         }
         return size;
     }
+
+    private static int getInteger() {
+        try {
+            if (scanner.hasNextInt())
+                return scanner.nextInt();
+            else
+                throw new InputMismatchException();
+        }
+        catch (InputMismatchException error) {
+            System.out.println(Message.INVALID_NUMBER_INPUT);
+            scanner.next();
+            return getInteger();
+        }
+    }
+
+}
 
 
 
