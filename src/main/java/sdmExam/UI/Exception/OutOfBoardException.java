4aefb0d2b0c5d822574518c1f986c0e242dec9a5
Stefano <stefanosimonetto98@gmail.com>
2021-01-20 16:02:51 +0100

    Created a new folder and a new class for UI exception

---
+++ src/main/java/sdmExam/UI/Exception/OutOfBoardException.java
@@ -0,0 +1,10 @@
+package sdmExam.UI.Exception;
+
+public class OutOfBoardException  extends  Exception{
+    public OutOfBoardException(String error)
+    {
+        super(error);
+    }
+
+}
+
--- src/main/java/sdmExam/UI/InputHandle.java
+++ src/main/java/sdmExam/UI/InputHandle.java
@@ -1,9 +1,14 @@
 package sdmExam.UI;
 
+import java.util.Scanner;
+
 public class InputHandle {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public int askSize() {
         System.out.println(Message.ASK_SIZE);
+
+
     }
 
 
