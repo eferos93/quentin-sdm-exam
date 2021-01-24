643b8f9baf8a5eb452abb9ba97689109408e811a
wilsonjefferson <pierpa.m.96@live.it>
2021-01-24 17:32:08 +0100

    ConsoleMessageWriter.askBoardSize added; ConsoleMessageWriter.askRowCoordinate added; ConsoleMessageWriter.askColumnCoordinate added.

--- src/main/java/sdmExam/UI/ConsoleMessageWriter.java
+++ src/main/java/sdmExam/UI/ConsoleMessageWriter.java
@@ -10,6 +10,18 @@ public class ConsoleMessageWriter {
 
     public static void blackPlayerPlayFirst() {System.out.println(Message.BLACK_PLAYS_FIRST);}
 
+    public static void askBoardSize() {
+        System.out.println(Message.ASK_SIZE);
+    }
+
+    public static void askRowCoordinate() {
+        System.out.println(Message.CHOOSE_ROW);
+    }
+
+    public static void askColumnCoordinate() {
+        System.out.println(Message.CHOOSE_COLUMN);
+    }
+
     public static void displayPlayer(Player player) {
         System.out.printf(Message.CURRENT_PLAYER, player.getName(), ConsoleStoneRepresentation.getStoneValue(player.getColor()));
     }
