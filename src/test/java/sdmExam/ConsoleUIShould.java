88de33efdac7ac36d796e7094dae923eafdcc680
wilsonjefferson <pierpa.m.96@live.it>
2021-01-24 18:07:56 +0100

    ConsoleUIShould.printCorrectlyIntersection refactor.

--- src/test/java/sdmExam/ConsoleUIShould.java
+++ src/test/java/sdmExam/ConsoleUIShould.java
@@ -1,21 +1,40 @@
 package sdmExam;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import sdmExam.UI.PrintBoard;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConsoleUIShould {
 
-    @Test
-    public void printCorrectlyIntersection() {
-        Intersection intersection = new Intersection(Position.in(1, 1), Stone.BLACK);
+    @TestFactory
+    Stream<DynamicTest> printCorrectlyIntersection() {
+
         ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
         System.setOut(new PrintStream(fakeStandardOutput));
-        PrintBoard.displayStone(intersection.getStone());
-        assertEquals("[B]", fakeStandardOutput.toString());
+
+        List<Intersection> intersections = List.of(
+                new Intersection(Position.in(1, 1), Stone.BLACK),
+                new Intersection(Position.in(1, 2), Stone.WHITE),
+                Intersection.empty(Position.in(2, 2))
+        );
+
+        List<String> expectedOutput = List.of("[B]", "[W]", "[ ]");
+
+        return intersections.stream().map(intersection ->
+                DynamicTest.dynamicTest("Checking " + intersection,
+                () -> {
+                    int index = intersections.indexOf(intersection);
+                    PrintBoard.displayStone(intersection.getStone());
+                    assertEquals(expectedOutput.get(index), fakeStandardOutput.toString());
+                    fakeStandardOutput.reset();
+                })
+        );
     }
 }
