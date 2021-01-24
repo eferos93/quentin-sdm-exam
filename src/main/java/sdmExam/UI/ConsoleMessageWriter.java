a45fd46d6aeaf770fa30301f631df71a3a0d634f
wilsonjefferson <pierpa.m.96@live.it>
2021-01-24 17:24:35 +0100

    ConsoleMessageWriter.notifyPass added; ConsoleMessageWriter.notifyPieRule; Message class updated.

--- src/main/java/sdmExam/UI/ConsoleMessageWriter.java
+++ src/main/java/sdmExam/UI/ConsoleMessageWriter.java
@@ -14,9 +14,18 @@ public class ConsoleMessageWriter {
         System.out.printf(Message.CURRENT_PLAYER, player.getName(), ConsoleStoneRepresentation.getStoneValue(player.getColor()));
     }
 
+    public static void notifyPass(){
+        System.out.println(Message.PASS_TURN);
+    }
+
+    public static void notifyPieRule(Player player1, Player player2){
+        System.out.printf(Message.PIE, player1.getName(), ConsoleStoneRepresentation.getStoneValue(player1.getColor()),
+                player2.getName(), ConsoleStoneRepresentation.getStoneValue(player2.getColor()));
+    }
+
     public static void notifyWinner(Player player) {
         System.out.printf(Message.END_GAME, player.getName());
     }
 
-    public static void askPie() {System.out.println(Message.PIE);}
+    public static void askPie() {System.out.println(Message.QUERY_PIE);}
 }
