bd5766db0d11fe5386ca2e18158938f6c0cef9aa
wilsonjefferson <pierpa.m.96@live.it>
2021-02-22 09:32:36 +0100

    Dead Code removed: OutOfBoardException and PieException eliminated.

--- src/main/java/quentin/exceptions/IllegalMoveException.java
+++ src/main/java/quentin/exceptions/IllegalMoveException.java
@@ -4,7 +4,7 @@ import quentin.core.Position;
 
 public class IllegalMoveException extends Exception {
     public IllegalMoveException(Position position){
-        super("Player cannot put a stone diagonally adjacent to another stone of the same colour this" + position +
+        super("Player cannot put a stone diagonally adjacent to another stone of the same colour in this " + position +
                 " without a colour alike orthogonally adjacent.");
     }
 }
