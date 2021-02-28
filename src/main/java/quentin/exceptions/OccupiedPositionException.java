4ac7f9ab0bbd0a413f7aaf700d474b56d9ed4dc2
eferos93 <eros.fabrici@gmail.com>
2021-02-28 12:37:10 +0100

    format code

--- src/main/java/quentin/exceptions/OccupiedPositionException.java
+++ src/main/java/quentin/exceptions/OccupiedPositionException.java
@@ -3,7 +3,6 @@ package quentin.exceptions;
 import quentin.core.Position;
 
 public class OccupiedPositionException extends QuentinException {
-
     public OccupiedPositionException(Position position){
         super(position + " is already occupied.");
     }
